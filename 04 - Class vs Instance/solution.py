202f33cf81f007cd557f983e7afbb670629b72f1
Andrew Erb <ase111@gmail.com>
2017-04-06 23:21:18 -0400

    Days 4 & 5

---
+++ 04 - Class vs Instance/solution.py
@@ -0,0 +1,20 @@
+#Day 04 - Class vs Instance
+
+class Person:
+    def __init__(self,initialAge):
+        # Add some more code to run some checks on initialAge
+        x = 0
+        if initialAge < 0:
+            age = 0
+            print "Invalid age"
+        else: 
+            age = initialAge
+    def amIOld(self):
+        # Do some computations in here and print out the correct statement to the console
+        x = 0
+        
+    def yearPasses(self):
+        # Increment the age of the person in here      
+        x = 0
+
+    age = 0
