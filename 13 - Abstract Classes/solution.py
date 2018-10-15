6e741dfdb9f70deb0f255560b9a64c96d9af7944
Andrew Erb <ase111@gmail.com>
2018-10-15 03:15:07 -0400

    13 - Abstract Classes

--- 13 - Abstract Classes/solution.py
+++ 13 - Abstract Classes/solution.py
@@ -1,7 +1,26 @@
-
+#Provided code
+from abc import ABCMeta, abstractmethod
+class Book(object, metaclass=ABCMeta):
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author   
+    @abstractmethod
+    def display(): pass
 
 #Write MyBook class
 class MyBook(Book):
     def __init__(self,title,author,price):
-        pass
-    def display(): pass
+        self.title=title
+        self.author=author
+        self.price=price
+    def display(self): 
+        print("Title: ", self.title)
+        print("Author: ", self.author)
+        print("Price: ", self.price)
+
+#Provided code
+title=input()
+author=input()
+price=int(input())
+new_novel=MyBook(title,author,price)
+new_novel.display()
