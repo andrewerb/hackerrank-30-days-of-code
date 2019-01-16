08c6f240c5d7301d9d76ebbdd14e20ce6422076d
Andrew Erb <ase111@gmail.com>
2019-01-16 11:08:47 -0500

    #18 queues and stacks

--- 18 - Queues and Stacks/solution.py
+++ 18 - Queues and Stacks/solution.py
@@ -2,29 +2,40 @@
 """
 
 import sys
+from collections import deque
 
 class Solution:
     """ Take a string s. Enqueue s into a queue and push s into a stack.
         If both match up when popped, then s is a palindrome.
 
         Not bothering to type check/cast for this implementation.
+        
+        s is composed of lowercase English letters. No need to fix casing.
     """
     # Write your code here
     def __init__(self):
-        self.stack = None
-        self.queue = None
+        self.stack = list()
+        self.queue = deque()
 
     def pushCharacter(self, ch):
-        pass
+        """ pushes a character onto stack.
+        """
+        self.stack.append(ch)
 
     def enqueueCharacter(self, ch):
-        pass
+        """ enqueues a character in queue
+        """
+        self.queue.append(ch)
 
     def popCharacter(self):
-        pass
+        """ pops and returns the character at the top of stack
+        """
+        return self.stack.pop()
 
     def dequeueCharacter(self):
-        pass
+        """ dequeues and returns the first character in queue
+        """
+        return self.queue.popleft()
 
     # end of custom code
 ###########################################
