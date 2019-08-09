b56b6ac080dc871f2b43115aaa83f2fac814ac78
Andrew Erb <ase111@gmail.com>
2019-08-09 02:29:09 -0400

    #22 - BSTs - BST Height function example

---
+++ 22 - Binary Search trees/solution.py
@@ -0,0 +1,48 @@
+""" 22 - BSTs
+"""
+
+class Node:
+    def __init__(self, data):
+        self.right = self.left = None
+        self.data = data
+
+
+class Solution:
+    def insert(self, root, data):
+        if root == None:
+            return Node(data)
+        else:
+            if data <= root.data:
+                cur = self.insert(root.left, data)
+                root.left = cur
+            else:
+                cur = self.insert(root.right, data)
+                root.right = cur
+        return root
+
+    def getHeight(self, root):
+        #Write your code here
+
+        # if not (l or r ):
+        #     return 0
+        
+        r_height = 0
+        l_height = 0
+
+        if root.right:
+            r_height = self.getHeight(root.right) + 1
+
+        if root.left:
+            l_height = self.getHeight(root.left) + 1
+
+        return max(r_height, l_height)
+
+
+T = int(input())
+myTree = Solution()
+root = None
+for i in range(T):
+    data = int(input())
+    root = myTree.insert(root, data)
+height = myTree.getHeight(root)
+print(height)
